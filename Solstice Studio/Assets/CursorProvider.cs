36ef44dd6a9e6d6bc4ac37f8f8b9b2acc25423ab
Daher Alfawares <daher.alfawares@live.com>
2016-12-02 10:32:46 -0800

    adding

---
+++ Solstice Studio/Assets/CursorProvider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorProvider : MonoBehaviour {
+
+    [Tooltip("Set the cursor object set this on the parent of an object group.")]
+    public GameObject HoverCursor;
+
+    public void ShowCursor( Vector3 Position, Vector3 Normal)
+    {
+        HoverCursor.SetActive(true);
+        HoverCursor.transform.position = Position;
+        HoverCursor.transform.up = Normal;
+    }
+}
